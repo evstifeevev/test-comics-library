ae8f00092854a8cbbacdaf4776dd218d9f5927bc
evgenii_evstifeev <e.v.evstifeev@gmail.com>
2022-06-13 20:32:08 +0400

    TCL-2: added draft entities

---
+++ Saratov.Evstifeev.TestComicsLibrary/Saratov.Evstifeev.TestComicsLibrary.Entities.Static/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace Saratov.Evstifeev.TestComicsLibrary.Entities.Static
+{
+    public enum UserRole
+    {
+        User = 0,
+        Moderator = 1,
+        Administrator = 2,
+        SuperAdministrator = 4,
+    }
+}
